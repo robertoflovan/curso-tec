c6654bb41c03d9321e6e9aa668b6a25fc3d2858c
Roberto <roberto_flovan@hotmail.com>
2015-10-31 12:31:42 -0600

    Add: estilo de la barra lateral

---
+++ clase2-maquetacion-intermedio/public/js/app.js
@@ -0,0 +1,41 @@
+var a = "A";
+(function){
+	var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+var mapOptions = {
+  zoom: 4,
+  center: myLatlng
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    title:"Hello World!"
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*function tipo1(){
+	console.log("function tipo 1")
+}
+
+tipo1();
+
+var tipo2 = function(){
+	console.log("function tipo 2");
+}
+
+tipo2();*/

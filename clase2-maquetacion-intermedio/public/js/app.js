19316a78b807202d917d396947962ce25fbc6863
Roberto <roberto_flovan@hotmail.com>
2015-11-07 10:05:08 -0600

    Add: targeta en el area de contenido

--- clase2-maquetacion-intermedio/public/js/app.js
+++ clase2-maquetacion-intermedio/public/js/app.js
@@ -1,4 +1,4 @@
-var a = "A";
+/*var a = "A";
 (function){
 	var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
 var mapOptions = {
@@ -10,7 +10,7 @@ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 var marker = new google.maps.Marker({
     position: myLatlng,
     title:"Hello World!"
-})();
+})();*/
 
 
 

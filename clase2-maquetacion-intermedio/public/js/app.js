091dc9834088c81139061697a605c38a869a1a02
Roberto <roberto_flovan@hotmail.com>
2015-11-07 12:22:54 -0600

    Add: popup contratar

--- clase2-maquetacion-intermedio/public/js/app.js
+++ clase2-maquetacion-intermedio/public/js/app.js
@@ -1,8 +1,8 @@
-/*var a = "A";
-(function){
-	var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+
+(function(){
+	var myLatlng = new google.maps.LatLng(25.426694, -100.995402);
 var mapOptions = {
-  zoom: 4,
+  zoom: 14,
   center: myLatlng
 }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -10,10 +10,28 @@ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 var marker = new google.maps.Marker({
     position: myLatlng,
     title:"Hello World!"
-})();*/
+});
 
 
+marker.setMap(map);
 
+
+var btnpopup = document.getElementById('btnpopup');
+var popup = document.getElementById('popup');
+var closePopup = document.getElementById('closePopup');
+
+btnpopup.addEventListener('click', onClickPopup);
+closePopup.addEventListener('click', onClickPopup)
+
+
+function onClickPopup(){
+	popup.classList.toggle('Popup--visible');
+}
+/*function onClickClose(){
+	popup.classList.remove('popup--visible');
+}*/
+
+})();
 
 
 
